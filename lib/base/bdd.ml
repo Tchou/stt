3f75ee1d2276c8772ac73f0211cdbb2e5863cb77
Kim Nguyễn <kim@nguyen.vg>
2023-10-16 20:05:17 +0200

    Rewrite BDD application function.

--- lib/base/bdd.ml
+++ lib/base/bdd.ml
@@ -4,9 +4,9 @@ type ('a, 't) bdd =
   | Node of (int * 'a * ('a, 't) bdd * ('a, 't) bdd)
 
 module Make (X : Common.T) (L : Sigs.PreSet) = struct
-  type t = (X.t, L.t) bdd
   type atom = X.t
   type leaf = L.t
+  type t = (atom, leaf) bdd
 
   let equal t1 t2 = t1 == t2
 
@@ -23,40 +23,42 @@ module Make (X : Common.T) (L : Sigs.PreSet) = struct
     | False -> fprintf fmt "@[False@]"
     | True (id, l) -> fprintf fmt "@[True (%d,@[%a@])@]" id L.pp l
     | Node (id, var, low, hi) ->
-        fprintf fmt "@[Node (@[%d,@ @[%a@],@ @[%a@],@ @[%a@]@])@]" id X.pp var pp
-          low pp hi
+        fprintf fmt "@[Node (@[%d,@ @[%a@],@ @[%a@],@ @[%a@]@])@]" id X.pp var
+          pp low pp hi
 
-  module H = Hashtbl.Make (struct
+  module HNode = Hashtbl.Make (struct
     type nonrec t = t
 
-    let rec equal b1 b2 =
+    let rec equal : t -> t -> bool =
+     fun b1 b2 ->
       b1 == b2
       ||
       match b1, b2 with
+      | False, False -> true
+      | True (_, l1), True (_, l2) -> L.equal l1 l2
       | Node (_, var1, low1, hi1), Node (_, var2, low2, hi2) ->
           X.equal var1 var2 && equal low1 low2 && equal hi1 hi2
-      | True (_, t1), True (_, t2) -> L.equal t1 t2
       | _ -> false
 
     let hash = function
       | False -> 0
-      | True (_, t) -> L.hash t
+      | True (_, l) -> L.hash l
       | Node (_, var, low, hi) ->
           let hl = hash low in
           let hh = hash hi in
           X.hash var + ((hl lsl 4) + hl) + ((hh lsl 16) + hl)
   end)
 
-  let memo = H.create 16
+  let memo_node = HNode.create 16
   let uid = ref 1
 
   let true_ t =
     if L.is_empty t then False
     else
       let n = True (!uid, t) in
-      try H.find memo n with
+      try HNode.find memo_node n with
       | Not_found ->
-          H.add memo n n;
+          HNode.add memo_node n n;
           incr uid;
           n
 
@@ -64,16 +66,16 @@ module Make (X : Common.T) (L : Sigs.PreSet) = struct
     if low == hi then low
     else
       let n = Node (!uid, var, low, hi) in
-      try H.find memo n with
+      try HNode.find memo_node n with
       | Not_found ->
-          H.add memo n n;
+          HNode.add memo_node n n;
           incr uid;
           n
 
   let empty = False
-  let is_empty t = t == False
+  let is_empty = function False -> true | _ -> false
   let any = true_ L.any
-  let is_any l = l == any
+  let is_any = function True (_, l) -> L.is_any l | _ -> false
   let atom x = node x ~low:empty ~hi:any
   let leaf l = true_ l
 
@@ -82,50 +84,36 @@ module Make (X : Common.T) (L : Sigs.PreSet) = struct
     | True (_, t) -> true_ (L.neg t)
     | Node (_, var, low, hi) -> node var ~low:(neg low) ~hi:(neg hi)
 
-  let rec apply base_op t1 t2 =
-    base_op
-      (fun t1 t2 ->
-        match t1, t2 with
-        | Node (_, var, low, hi), (True _ as t)
-         |(True _ as t), Node (_, var, low, hi) ->
-            node var ~low:(apply base_op low t) ~hi:(apply base_op hi t)
-        | Node (_, var1, low1, hi1), Node (_, var2, low2, hi2) ->
-            let c = X.compare var1 var2 in
-            if c = 0 then
-              node var1 ~low:(apply base_op low1 low2)
-                ~hi:(apply base_op hi1 hi2)
-            else if c < 0 then
-              node var1 ~low:(apply base_op low1 t2) ~hi:(apply base_op hi1 t2)
-            else
-              node var2 ~low:(apply base_op t1 low2) ~hi:(apply base_op t1 hi2)
-        | _ -> assert false)
-      t1 t2
-
-  let base_cup k t1 t2 =
-    if t1 == t2 then t1
+  let rec apply base_eq base_f_o base_o_f base_t_t t1 t2 =
+    if t1 == t2 then base_eq t1 t2
     else
       match t1, t2 with
-      | False, t | t, False -> t
-      | True (_, l1), True (_, l2) -> true_ (L.cup l1 l2)
-      | _ -> k t1 t2
+      | False, _ -> base_f_o t2
+      | _, False -> base_o_f t1
+      | True (_, l1), True (_, l2) -> true_ (base_t_t l1 l2)
+      | Node (_, var, low, hi), (True _ as t)
+       |(True _ as t), Node (_, var, low, hi) ->
+          node var
+            ~low:(apply base_eq base_f_o base_o_f base_t_t low t)
+            ~hi:(apply base_eq base_f_o base_o_f base_t_t hi t)
+      | Node (_, var1, low1, hi1), Node (_, var2, low2, hi2) ->
+          let c = X.compare var1 var2 in
+          if c = 0 then
+            node var1
+              ~low:(apply base_eq base_f_o base_o_f base_t_t low1 low2)
+              ~hi:(apply base_eq base_f_o base_o_f base_t_t hi1 hi2)
+          else if c < 0 then
+            node var1
+              ~low:(apply base_eq base_f_o base_o_f base_t_t low1 t2)
+              ~hi:(apply base_eq base_f_o base_o_f base_t_t hi1 t2)
+          else
+            node var2
+              ~low:(apply base_eq base_f_o base_o_f base_t_t t1 low2)
+              ~hi:(apply base_eq base_f_o base_o_f base_t_t t1 hi2)
 
-  let base_cap k t1 t2 =
-    if t1 == t2 then t1
-    else
-      match t1, t2 with
-      | False, _ | _, False -> False
-      | True (_, l1), True (_, l2) -> true_ (L.cap l1 l2)
-      | _ -> k t1 t2
-
-  let base_diff k t1 t2 =
-    if t1 == t2 then False
-    else
-      match t1, t2 with
-      | (False as t), _ | t, False -> t
-      | True (_, l1), True (_, l2) -> true_ (L.diff l1 l2)
-      | _ -> k t1 t2
-
-  let cup = apply base_cup
-  let cap = apply base_cap
-  let diff = apply base_diff
+  let ignore_false _ = False
+  let ignore_false2 _ _ = False
+  let cup = apply (fun t1 _ -> t1) Fun.id Fun.id L.cup
+  let cap = apply (fun t1 _ -> t1) ignore_false ignore_false L.cap
+  let diff = apply ignore_false2 ignore_false Fun.id L.diff
 end
