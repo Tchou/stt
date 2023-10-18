2a77ca060e4c2d6f1e579378e8752179b03c657f
Kim Nguyễn <kim@nguyen.vg>
2023-10-18 08:37:26 +0200

    Simplify BDD expansion.

--- lib/base/bdd.ml
+++ lib/base/bdd.ml
@@ -1,15 +1,9 @@
 type ('a, 't) bdd =
   | False
-  | True of {
-      id : int;
-      leaf : 't;
-    }
-  | Node of {
-      id : int;
-      var : 'a;
-      low : ('a, 't) bdd;
-      hi : ('a, 't) bdd;
-    }
+  | True of { id : int; leaf : 't }
+  | Node of { id : int; var : 'a; low : ('a, 't) bdd; hi : ('a, 't) bdd }
+
+(* Only added in OCaml 4.13-4.14 *)
 
 let memoize f arg =
   let r = ref (fun () -> assert false) in
@@ -20,17 +14,24 @@ let memoize f arg =
        res);
   fun () -> !r ()
 
-let dnf_gen build_seq t =
-  let rec loop todo =
-    match todo with
-    | [] -> Seq.Nil
-    | (_, _, False) :: todo -> loop todo
-    | (pos, neg, True { leaf; _ }) :: todo ->
-        build_seq (pos, neg) leaf (memoize loop todo)
-    | (pos, neg, Node { var; low; hi; _ }) :: todo ->
-        loop ((var :: pos, neg, hi) :: (pos, var :: neg, low) :: todo)
-  in
-  memoize loop [ [], [], t ]
+let rec append_seq s1 (s2 : 'a Seq.t) () =
+  match s1 () with
+    Seq.Nil -> s2 ()
+  | Seq.Cons(e, ss1) ->
+    Seq.Cons(e, memoize (append_seq ss1 s2)())
+
+let rec map_seq f s () =
+  match s () with
+    Seq.Nil -> Seq.Nil
+  | Seq.Cons (e, ss) ->
+    Seq.Cons (f e, memoize (map_seq f ss)())
+
+let rec flat_map_seq f s () =
+  match s () with
+    Seq.Nil -> Seq.Nil
+  | Seq.Cons(e, ss) ->
+    (memoize (append_seq (f e) (memoize (flat_map_seq f ss)())) ()) ()
+
 
 let ignore_first x _ = x
 let ignore_false _ = False
@@ -51,31 +52,31 @@ module Make (X : Common.T) (L : Sigs.PreSet) = struct
     | False -> fprintf fmt "@[False@]"
     | True { id; leaf } -> fprintf fmt "@[True (%d,@[%a@])@]" id L.pp leaf
     | Node { id; var; low; hi } ->
-        fprintf fmt "@[Node (@[%d,@ @[%a@],@ @[%a@],@ @[%a@]@])@]" id X.pp var
-          pp low pp hi
+      fprintf fmt "@[Node (@[%d,@ @[%a@],@ @[%a@],@ @[%a@]@])@]" id X.pp var
+        pp low pp hi
 
   module HNode = Hashtbl.Make (struct
-    type nonrec t = t
+      type nonrec t = t
 
-    let rec equal : t -> t -> bool =
-     fun b1 b2 ->
-      b1 == b2
-      ||
-      match b1, b2 with
-      | False, False -> true
-      | True r1, True r2 -> L.equal r1.leaf r2.leaf
-      | Node n1, Node n2 ->
+      let rec equal : t -> t -> bool =
+        fun b1 b2 ->
+        b1 == b2
+        ||
+        match b1, b2 with
+        | False, False -> true
+        | True r1, True r2 -> L.equal r1.leaf r2.leaf
+        | Node n1, Node n2 ->
           X.equal n1.var n2.var && equal n1.low n2.low && equal n1.hi n2.hi
-      | _ -> false
+        | _ -> false
 
-    let hash = function
-      | False -> 0
-      | True { leaf; _ } -> L.hash leaf
-      | Node { var; low; hi; _ } ->
+      let hash = function
+        | False -> 0
+        | True { leaf; _ } -> L.hash leaf
+        | Node { var; low; hi; _ } ->
           let hl = hash low in
           let hh = hash hi in
           X.hash var + ((hl lsl 4) + hl) + ((hh lsl 16) + hl)
-  end)
+    end)
 
   let memo_node = HNode.create 16
   let uid = ref 1
@@ -86,9 +87,9 @@ module Make (X : Common.T) (L : Sigs.PreSet) = struct
       let n = True { id = !uid; leaf } in
       try HNode.find memo_node n with
       | Not_found ->
-          HNode.add memo_node n n;
-          incr uid;
-          n
+        HNode.add memo_node n n;
+        incr uid;
+        n
 
   let node var ~low ~hi =
     if low == hi then low
@@ -96,9 +97,9 @@ module Make (X : Common.T) (L : Sigs.PreSet) = struct
       let n = Node { id = !uid; var; low; hi } in
       try HNode.find memo_node n with
       | Not_found ->
-          HNode.add memo_node n n;
-          incr uid;
-          n
+        HNode.add memo_node n n;
+        incr uid;
+        n
 
   let empty = False
   let is_empty = function False -> true | _ -> false
@@ -127,20 +128,20 @@ module Make (X : Common.T) (L : Sigs.PreSet) = struct
       | _, False -> op.o_f t1
       | True r1, True r2 -> true_ (op.t_t r1.leaf r2.leaf)
       | Node { var; low; hi; _ }, (True _ as t)
-       |(True _ as t), Node { var; low; hi; _ } ->
-          let low = apply op low t in
-          let hi = apply op hi t in
-          node var ~low ~hi
+      |(True _ as t), Node { var; low; hi; _ } ->
+        let low = apply op low t in
+        let hi = apply op hi t in
+        node var ~low ~hi
       | Node r1, Node r2 ->
-          let c = X.compare r1.var r2.var in
-          let var, l1, l2, h1, h2 =
-            if c = 0 then r1.var, r1.low, r2.low, r1.hi, r2.hi
-            else if c > 0 then r2.var, t1, r2.low, t1, r2.hi
-            else r1.var, r1.low, t2, r1.hi, t2
-          in
-          let low = apply op l1 l2 in
-          let hi = apply op h1 h2 in
-          node var ~low ~hi
+        let c = X.compare r1.var r2.var in
+        let var, l1, l2, h1, h2 =
+          if c = 0 then r1.var, r1.low, r2.low, r1.hi, r2.hi
+          else if c > 0 then r2.var, t1, r2.low, t1, r2.hi
+          else r1.var, r1.low, t2, r1.hi, t2
+        in
+        let low = apply op l1 l2 in
+        let hi = apply op h1 h2 in
+        node var ~low ~hi
 
   let cup t1 t2 =
     apply { eq = ignore_first; f_o = Fun.id; o_f = Fun.id; t_t = L.cup } t1 t2
@@ -158,22 +159,28 @@ module Make (X : Common.T) (L : Sigs.PreSet) = struct
   module Disj = List (Conj)
 
   let dnf t =
-    dnf_gen (fun (pos, neg) leaf seq -> Cons (((pos, neg), leaf), seq)) t
+    let rec loop todo =
+      match todo with
+      | [] -> Seq.Nil
+      | (_, _, False) :: todo -> loop todo
+      | (pos, neg, True { leaf; _ }) :: todo ->
+        Seq.Cons (((pos, neg), leaf), (memoize loop todo))
+      | (pos, neg, Node { var; low; hi; _ }) :: todo ->
+        loop ((var :: pos, neg, hi) :: (pos, var :: neg, low) :: todo)
+    in
+    memoize loop [ [], [], t ]
+
 end
 
 module MakeLevel2 (X : Common.T) (L : Sigs.Bdd) = struct
   include Make (X) (L)
 
+
+  let expand_dnf (x_atoms, leaf) =
+    let l_dnf = L.dnf leaf in
+    map_seq (fun (l_atoms, _) -> (x_atoms, l_atoms)) l_dnf
+
   let full_dnf (t : t) =
-    let rec loop_leaf x_atoms leaf_seq cont () =
-      match leaf_seq () with
-      | Seq.Nil -> loop cont ()
-      | Seq.Cons ((l_atoms, _), lleaf_seq) ->
-          Seq.Cons ((x_atoms, l_atoms), loop_leaf x_atoms lleaf_seq cont)
-    and loop cont =
-      match cont () with
-      | Seq.Nil -> Seq.empty
-      | Cons ((x_atoms, leaf), ccont) -> loop_leaf x_atoms (L.dnf leaf) ccont
-    in
-    loop (dnf t)
+    flat_map_seq expand_dnf (dnf t)
+
 end
