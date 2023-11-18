c83a63269f7bbefdf929614f0b4b50edd7d833c1
Kim Nguyễn <kim@nguyen.vg>
2023-11-18 10:14:28 +0100

    Add more type equalities in the BDD signatures to not make some concrete type abstracts when intanciating the functor.

--- core/base/bdd.mli
+++ core/base/bdd.mli
@@ -1,12 +1,14 @@
 module Make (X : Common.T) (L : Sigs.Set) : Sigs.Bdd
-  with type Leaf.t = L.t
-   and type atom = X.t
+  with type atom = X.t
+    and type Leaf.t = L.t
+    and type Leaf.elem = L.elem
 (** A functor to make Binary Decision Diagrams from the given
     [atom] and [leaf] components.
 *)
 
 module MakeLevel2 (X : Common.T) (L : Sigs.Bdd) : Sigs.Bdd2
   with type Leaf.t = L.t
+   and type Leaf.elem = L.elem
    and type LeafBdd.t = L.t
    and type LeafBdd.atom = L.atom
    and type atom = X.t
