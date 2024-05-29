23ae38f6aab27d53edddff4c89f1c1162f2e8e76
p9romain <romainperrod.pro9@gmail.com>
2024-05-29 10:07:16 +0200

    Change doc

--- syntax/automaton.mli
+++ syntax/automaton.mli
@@ -1,58 +1,59 @@
 module type S = sig
 
-  (* The letter type *)
   type lt
-  (* The automaton type *)
+  (** The letter type *)
   type t
+  (** The automaton type *)
 
-  (* The regexp module *)
   module R : Regexp.S
-  (* The regexp type *)
+  (** The regexp module *)
   type regexp
+  (** The regexp type *)
 
-  (* The empty automaton *)
   val empty : t
+  (** The empty automaton *)
 
-  (* [add_state automaton state] adds [state] in [automaton]. 
-    If it's already a state in [automaton], does nothing *)
   val add_state : t -> int -> t
-  (* [add_states automaton states] adds several states in [automaton] *)
+  (** [add_state automaton state] adds [state] in [automaton]. 
+    If it's already a state in [automaton], does nothing *)
   val add_states : t -> int list -> t
-  (* [add_trans automaton state1 letter state2] adds a transition labelled [letter] between [state1] and [state2] in [automaton].
+  (** [add_states automaton states] adds several states in [automaton] *)
+  val add_trans : t -> int -> lt -> int -> t
+  (** [add_trans automaton state1 letter state2] adds a transition labelled [letter] between [state1] and [state2] in [automaton].
 
     Raises : [state1] and [state2] must be [automaton]'s states.
     Raises : [l] must be in the [automaton]'s alphabet.
 
     If the transition is already in [automaton], does nothing *)
-  val add_trans : t -> int -> lt -> int -> t
-  (* [add_transitions automaton transitions] adds several transitions in [automaton] *)
   val add_transitions : t -> (int * lt * int) list -> t
-  (* [add_start automaton state] sets [state] as a start state in [automaton].
-    If it's already the case, does nothing
-
-    Raise : [state] must be an [automaton]'s state *)
+  (** [add_transitions automaton transitions] adds several transitions in [automaton] *)
   val add_start : t -> int -> t
-  (* [add_starts automaton states] sets several states as start states in [automaton] *)
-  val add_starts : t -> int list -> t
-  (* [add_end automaton state] sets [state] as an end state in [automaton].
+  (** [add_start automaton state] sets [state] as a start state in [automaton].
     If it's already the case, does nothing
 
     Raise : [state] must be an [automaton]'s state *)
+  val add_starts : t -> int list -> t
+  (** [add_starts automaton states] sets several states as start states in [automaton] *)
   val add_end : t -> int -> t
-  (* [add_ends automaton states] sets several states as end states in [automaton] *)
+  (** [add_end automaton state] sets [state] as an end state in [automaton].
+    If it's already the case, does nothing
+
+    Raise : [state] must be an [automaton]'s state *)
   val add_ends : t -> int list -> t
+  (** [add_ends automaton states] sets several states as end states in [automaton] *)
 
 
-  (* [is_deterministic automaton] checks if [automaton] is deterministic.
+  (* val is_deterministic : t -> bool *)
+  (** [is_deterministic automaton] checks if [automaton] is deterministic.
 
     A deterministic finite automaton (or DFA) follows these 3 rules :
     - Has one start state ;
     - Has no epsilon-transitions ;
     - For all states, there exists one and only one transition labelled by a same letter from it.
       For instance, if we have (1, "a", 2) and (1, "a", 3), then it isn't deterministic *)
-  (* val is_deterministic : t -> bool *)
 
-  (* [determinize automaton] returns an automaton which is the DFA version of [automaton].
+  (* val determinize : t -> t *)
+  (** [determinize automaton] returns an automaton which is the DFA version of [automaton].
     If [automaton] has n states, the DFA automaton has at most 2^n states.
 
     A deterministic finite automaton (or DFA) follows these 3 rules :
@@ -60,19 +61,18 @@ module type S = sig
     - Has no epsilon-transitions ;
     - For all states, there exists one and only one transition labelled by a same letter from it.
       For instance, if we have (1, "a", 2) and (1, "a", 3), then it isn't deterministic *)
-  (* val determinize : t -> t *)
 
-  (* [check_word automaton word] checks if [word] is recognized by [automaton] *)
   val check_word : t -> lt list -> bool
-  (* [to_regex_my automaton] returns the regex representing [automaton] using the McNaughton-Yamada method. 
-    The returned value might be unsimplified *)
+  (** [check_word automaton word] checks if [word] is recognized by [automaton] *)
   val to_regex_my : t -> regexp
-  (* [to_regex_bm automaton] returns the regex representing [automaton] using the Brzozowski-McCluskey method,
-    also know as the "state elimination method". 
+  (** [to_regex_my automaton] returns the regex representing [automaton] using the McNaughton-Yamada method. 
     The returned value might be unsimplified *)
   (* val to_regex_bm : t -> regexp *)
-  (* [from_regex reg alphabet] creates a NFA (Non-deterministic Finite Automaton) recognizing [reg] *)
+  (** [to_regex_bm automaton] returns the regex representing [automaton] using the Brzozowski-McCluskey method,
+    also know as the "state elimination method". 
+    The returned value might be unsimplified *)
   (* val from_regex : regexp -> lt list -> t *)
+  (** [from_regex reg alphabet] creates a NFA (Non-deterministic Finite Automaton) recognizing [reg] *)
 
 end
 
--- syntax/regexp.mli
+++ syntax/regexp.mli
@@ -1,38 +1,38 @@
 module type S = sig
 
-  (* The letter type *)
   type lt
+  (** The letter type *)
 
-  (* A simple implementation of a regex *)
   type t_simp
-  (* An extended version of the regex (with the option and plus) *)
+  (** A simple implementation of a regex *)
   type t_ext
+  (** An extended version of the regex (with the option and plus) *)
 
-  (* The empty regex *)
   val empty : t_simp
-  (* [is_empty r] checks if the regexp is the empty (not the 
-    empty word : ɛ ≠ ∅) *)
+  (** The empty regex *)
   val is_empty : t_simp -> bool
+  (** [is_empty r] checks if the regexp is the empty (not the 
+    empty word : ɛ ≠ ∅) *)
 
-  (* [letter l] creates a regex with the letter [l] *)
   val letter : lt -> t_simp
-  (* [concat r1 r2] creates a regex by concatenating [r1] with [r2] *)
+  (** [letter l] creates a regex with the letter [l] *)
   val concat : t_simp -> t_simp -> t_simp
-  (* [union r1 r2] creates a regex by uniting [r1] with [r2] *)
+  (** [concat r1 r2] creates a regex by concatenating [r1] with [r2] *)
   val union : t_simp -> t_simp -> t_simp
-  (* [star r] creates a regex with the Kleene star *)
+  (** [union r1 r2] creates a regex by uniting [r1] with [r2] *)
   val star : t_simp -> t_simp
+  (** [star r] creates a regex with the Kleene star *)
 
-  (* [simp_to_ext r] extends the simple regex [r], in order to simplify 
-    or print it *)
   val simp_to_ext : t_simp -> t_ext
+  (** [simp_to_ext r] extends the simple regex [r], in order to simplify 
+    or print it *)
 
-  (* [pp pp_lt r] cast the regex [r] into a string, using [pp_lt] to cast
-    the letter*)
   val pp : (Format.formatter -> lt -> unit) -> t_ext -> string
+  (** [pp pp_lt r] cast the regex [r] into a string, using [pp_lt] to cast
+    the letter*)
 
-  (* [simplify r] normalizes and simplfies the regex [r] as much as possible *)
   val simplify : t_ext -> t_ext
+  (** [simplify r] normalizes and simplfies the regex [r] as much as possible *)
 
 end
 
