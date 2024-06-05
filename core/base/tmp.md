2a665422ba574108ea95c18eb153d5962624e6c9
p9romain <romainperrod.pro9@gmail.com>
2024-06-05 14:48:39 +0200

    Before doing the variables (or a better version than now), need to change Pretty.t.Printer and add some new types to Stt.Base

---
+++ core/base/tmp.md
@@ -0,0 +1,11 @@
+type t = 
+  | Item of (formatter -> unit)
+  | Neg of t
+  | Union of t list
+  | Inter of t list
+  | Diff of t list
+
+Flatten les list
+Simplifier (sauf diff : pas toucher)
+
+Avec hypothèse que t.Item print que des types "atomiques"
--- syntax/automaton.mli
+++ syntax/automaton.mli
@@ -42,38 +42,11 @@ module type S = sig
   val add_ends : t -> int list -> t
   (** [add_ends automaton states] sets several states as end states in [automaton] *)
 
-
-  (* val is_deterministic : t -> bool *)
-  (** [is_deterministic automaton] checks if [automaton] is deterministic.
-
-    A deterministic finite automaton (or DFA) follows these 3 rules :
-    - Has one start state ;
-    - Has no epsilon-transitions ;
-    - For all states, there exists one and only one transition labelled by a same letter from it.
-      For instance, if we have (1, "a", 2) and (1, "a", 3), then it isn't deterministic *)
-
-  (* val determinize : t -> t *)
-  (** [determinize automaton] returns an automaton which is the DFA version of [automaton].
-    If [automaton] has n states, the DFA automaton has at most 2^n states.
-
-    A deterministic finite automaton (or DFA) follows these 3 rules :
-    - Has one start state ;
-    - Has no epsilon-transitions ;
-    - For all states, there exists one and only one transition labelled by a same letter from it.
-      For instance, if we have (1, "a", 2) and (1, "a", 3), then it isn't deterministic *)
-
   val check_word : t -> lt list -> bool
   (** [check_word automaton word] checks if [word] is recognized by [automaton] *)
   val to_regex_my : t -> regexp
   (** [to_regex_my automaton] returns the regex representing [automaton] using the McNaughton-Yamada method. 
     The returned value might be unsimplified *)
-  (* val to_regex_bm : t -> regexp *)
-  (** [to_regex_bm automaton] returns the regex representing [automaton] using the Brzozowski-McCluskey method,
-    also know as the "state elimination method". 
-    The returned value might be unsimplified *)
-  (* val from_regex : regexp -> lt list -> t *)
-  (** [from_regex reg alphabet] creates a NFA (Non-deterministic Finite Automaton) recognizing [reg] *)
-
 end
 
 module Make (Lt : Letter.Letter) : S with type lt = Lt.t 
