047fc94b1ef5799d21ab1aafe7b68db66fc00c30
Kim Nguyễn <kim.nguyen@lri.fr>
2024-07-11 13:55:15 +0200

    Tweak the priorities a bit.
    With lower priorities for Regexp operators (;, *, +, ?) the pretty printer will
    choose to not print parentheses when encountering:
    
      (x & y)*
    
    since the priorities of * are lower than those of &
    and then x & y * is not valid syntax, since it mixes a type (x) and a regexp (y*).
    We rise the priorities of those operators above negation. The priority for | (in regexp)
    remains the same as the one of | (in types) since even if we choose to print one or the
    other, it will be the same symbol, and reparsed correctly.

--- syntax/prio.ml
+++ syntax/prio.ml
@@ -2,9 +2,9 @@ type t = int
 
 let lowest : t = 0
 
-let pr_printer : t = 8
-let pr_pair : t = 8
-let pr_apply : t = 8
+let pr_printer : t = 11
+let pr_pair : t = 11
+let pr_apply : t = 11
 let pr_neg : t = 7
 let pr_cap : t = 6
 let pr_diff : t = 6
@@ -12,10 +12,10 @@ let pr_cup : t = 3
 let pr_arrow : t = 2
 let pr_rec : t = 1
 
-let re_star : t = 5
-let re_plus : t = 5
-let re_option : t = 5
-let re_concat : t = 4
+let re_star : t = 9
+let re_plus : t = 9
+let re_option : t = 9
+let re_concat : t = 8
 let re_union : t = 3
 
 let compare = Int.compare
